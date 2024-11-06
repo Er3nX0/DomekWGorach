28bc8179f58e5e06071c10d11d0f95268eddf92d
Er3nX0 <d8aniel8@outlook.com>
2024-11-06 22:52:52 +0100

    Dodanie funkcji do przyciskow

--- app/src/main/java/com/example/domekwgorach/MainActivity.java
+++ app/src/main/java/com/example/domekwgorach/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.domekwgorach;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView TextViewPolubienia;
+    private Button buttonPolub;
+    private Button buttonUsun;
+    private int polubienia = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        TextViewPolubienia = findViewById(R.id.polubienia);
+        buttonPolub = findViewById(R.id.polub);
+        buttonUsun = findViewById(R.id.usun);
 
+        buttonPolub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    polubienia++;
+                    TextViewPolubienia.setText(polubienia + " polubień");
+            }
+        });
+        buttonUsun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (polubienia>0){
+                    polubienia--;
+                    TextViewPolubienia.setText(polubienia + " polubień");
+                }
+            }
+        });
     }
 }
